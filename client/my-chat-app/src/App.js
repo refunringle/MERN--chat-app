5a19b1723a398e02afca8aa276bdaef6cf30713a
refun-empress <Refun@empressinfotech.com>
2022-12-12 16:10:08 +0530

    bad err

--- client/my-chat-app/src/App.js
+++ client/my-chat-app/src/App.js
@@ -5,33 +5,17 @@ import Chat from './Pages/Chat/Chat';
 import Register from './Pages/Register/Register';  
 
 
+
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<Chat/>} />
-        <Route exact path='/login' element={<Login/>} />
-        <Route exact path='/register' element={<Register/>} />
+        <Route exact path='/Login' element={<Login/>} />
+        <Route exact path='/Register' element={<Register/>} />
       </Routes>
       </BrowserRouter>
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload..
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 9e69544347d13a774e3435c39a99c3c9dde57ac1
     </div>
   );
 }
