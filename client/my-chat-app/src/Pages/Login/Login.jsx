c571efd80318601f8ad0fb315df25be9d88f35bf
Refun <refunfk@gmail.com>
2022-12-12 23:17:14 +0530

    added login template

--- client/my-chat-app/src/Pages/Login/Login.jsx
+++ client/my-chat-app/src/Pages/Login/Login.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import './login.css'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card">
+              <form className="box">
+                <h1>Login</h1>
+                <p className="text-muted">
+                  Please enter your login and password!</p>
+                <input type="text" name="" placeholder="Username"/>
+                <input type="password" name="" placeholder="Password"/>
+                <a className="forgot text-muted" href="#">Sign up</a>
+                <input type="submit" name="" value="Login" href="#"/>
+                <div className="col-md-12">
+                  <ul className="social-network social-circle">
+                    <li>
+                      <a href="#" className="icoFacebook" title="Facebook">
+                        <i className="fab fa-facebook-f"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="icoTwitter" title="Twitter">
+                        <i className="fab fa-twitter"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="#" className="icoGoogle" title="Google +">
+                        <i className="fab fa-google-plus"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
