44506d58b717d3bfe9e32f320fa69d12a70a3523
refun-empress <Refun@empressinfotech.com>
2022-12-14 18:19:36 +0530

    get data from FE to BE

---
+++ server/Controlers/userControles.js
@@ -0,0 +1,12 @@
+
+
+const register = (req, res) => {
+
+  console.log("jfjfyu");
+  res.send("hiyooo");
+  console.log(req.body);
+};
+
+module.exports = {
+  register
+};
--- server/index.js
+++ server/index.js
@@ -1,36 +1,46 @@
 const express = require("express");
 const cors = require("cors");
-//const dotenv = require ('dotenv')
 const mongoose = require("mongoose");
+var userRegister = require("./Routes/userRoutes");
 
 const app = express();
 
 app.use(express.json());
 
-require("dotenv").config()
+require("dotenv").config();
 
+app.use(
+  (req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    next();
+  },
+  cors({
+    credentials: true,
+  })
+);
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-}, 
-cors({
-  credentials: true
-}));
+// app.use("/", (req ,res) =>{
+//     res.send('hi')
+// });
+//pp.use('/register', function(){userRegister});
+// app.use("/hello", (req, res) => {
+//   res.send("Hellllo")
+// })
 
-app.use("/", (req ,res) =>{
-    res.send('hi')
-});
+app.use("", userRegister);
 
-
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 mongoose
   .connect(
-    process.env.MONGO_URI,  {
-        useNewUrlParser: true,
-      },
+    process.env.MONGO_URI,
+    {
+      useNewUrlParser: true,
+    },
     console.log("MongoDB Connected Successfully....")
   )
   .then(() => {
